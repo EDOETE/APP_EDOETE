3b1676fdb86250de338238f3fedb85044ef41f3b
edote <ablayesowsidibe@gmail.com>
2025-04-04 17:29:19 +0200

    Mon message

--- app.R
+++ app.R
@@ -1,12 +1,11 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
-library(shiny)
+#install.packages("rsconnect")
+#install.packages("bs4Dash")
+#install.packages("leaflet")
+
+#depploement shiny
+library(rsconnect)
+rsconnect::deployApp()
+
 # Run the application 
 shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -1,23 +1,29 @@
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+library(shiny)
+library(leaflet)
+library(bs4Dash)
+
+ui <- dashboardPage(
+  header = dashboardHeader(
+    title = dashboardBrand("Enquête Terminale Embrun", color = "primary")
+  ),
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  sidebar = dashboardSidebar(
+    skin = "light",
+    sidebarMenu(
+      menuItem("Accueil", tabName = "accueil", icon = icon("home")),
+      menuItem("Résultats globaux", tabName = "globaux", icon = icon("chart-bar")),
+      menuItem("Par sous-groupes", tabName = "groupes", icon = icon("users")),
+      menuItem("Cartographie", tabName = "carto", icon = icon("map"))
+    )
+  ),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+  body = dashboardBody(
+    tabItems(
+      tabItem(tabName = "accueil", h2("Bienvenue sur l'application")),
+      
+      tabItem(tabName = "globaux", h2("Bienvenue sur globaux")),
+      tabItem(tabName = "groupes", h2("Bienvenue sur groupes")),
+      tabItem(tabName = "carto", h2("Bienvenue sur la carte"))
     )
   )
 )
-

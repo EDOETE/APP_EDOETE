fed7a1ef77e27a18734f167480ddd77301333394
edote <ablayesowsidibe@gmail.com>
2025-04-07 16:22:11 +0200

    Mon premier commit

--- server.R
+++ server.R
@@ -1,5 +1,10 @@
 server <- function(input, output, session) {
+ 
+  
+  
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = 6.5, lat = 44.5, zoom = 10)
+  })
 }
-  
-  
-  
--- ui.R
+++ ui.R
@@ -18,12 +18,67 @@ ui <- dashboardPage(
   ),
   
   body = dashboardBody(
+    tags$head(
+      tags$style(HTML("
+        .carousel-inner img {
+          width: 100%;
+          height: 400px;
+          object-fit: cover;
+        }
+      "))
+    ),
+    
     tabItems(
-      tabItem(tabName = "accueil", h2("Bienvenue sur l'application")),
+      tabItem(tabName = "accueil",
+              h2("Bienvenue sur l'application"),
+              
+              # Carousel Bootstrap
+              tags$div(
+                id = "myCarousel",
+                class = "carousel slide",
+                `data-ride` = "carousel",
+                
+                # Indicateurs (les petits points)
+                tags$ol(class = "carousel-indicators",
+                        tags$li(`data-target` = "#myCarousel", `data-slide-to` = "0", class = "active"),
+                        tags$li(`data-target` = "#myCarousel", `data-slide-to` = "1"),
+                        tags$li(`data-target` = "#myCarousel", `data-slide-to` = "2")
+                ),
+                
+                # Contenu des slides
+                tags$div(class = "carousel-inner",
+                         tags$div(class = "carousel-item active",
+                                  tags$img(src = "image1.png", class = "d-block w-100")),
+                         tags$div(class = "carousel-item",
+                                  tags$img(src = "https://argouges.ent.auvergnerhonealpes.fr/lectureFichiergw.do?ID_FICHIER=7633", class = "d-block w-100")),
+                         tags$div(class = "carousel-item",
+                                  tags$img(src = "https://th.bing.com/th/id/OIP.ONekcOof-Sd5AdoBBJp6iQHaEK?w=1920&h=1080&rs=1&pid=ImgDetMain.png", class = "d-block w-100"))
+                ),
+                
+                # Contrôles gauche/droite
+                tags$a(class = "carousel-control-prev", href = "#myCarousel", role = "button", `data-slide` = "prev",
+                       tags$span(class = "carousel-control-prev-icon", `aria-hidden` = "true"),
+                       tags$span(class = "sr-only", "Précédent")),
+                
+                tags$a(class = "carousel-control-next", href = "#myCarousel", role = "button", `data-slide` = "next",
+                       tags$span(class = "carousel-control-next-icon", `aria-hidden` = "true"),
+                       tags$span(class = "sr-only", "Suivant"))
+              )
+      ),
       
-      tabItem(tabName = "globaux", h2("Bienvenue sur globaux")),
+      tabItem(tabName = "globaux",
+              h2("Bienvenue sur globaux"),
+              tabsetPanel(
+                tabPanel("Accueil", h3("Bienvenue !"), p("Ceci est l'onglet d'accueil.")),
+                tabPanel("Analyse", h3("Analyse des données"), plotOutput("plot")),
+                tabPanel("À propos", h3("Infos"), p("Créé avec amour en R Shiny."))
+              )
+      ),
+      
       tabItem(tabName = "groupes", h2("Bienvenue sur groupes")),
       tabItem(tabName = "carto", h2("Bienvenue sur la carte"))
     )
   )
 )
+
+
